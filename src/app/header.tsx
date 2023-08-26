b2554814c0edfdf8c18c1f06f77c70ea21c4f9d6
Jesús Villegas <villegasvjd@gmail.com>
2023-08-25 22:40:55 -0400

    update 25-08-23

---
+++ src/app/header.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from '../../app/page.module.css'
+import Menu from './menu'
+
+export default function Header() {
+  return (
+    <main >
+      <Menu/>
+    </main>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+import Header from './header'
+
+export default function Home() {
+  return (
+    <main >
+      <Header/>
+    </main>
+  )
+}
