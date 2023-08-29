70e4bf025c928b5bda5b67cb450598bee4060697
Jesús Villegas <villegasvjd@gmail.com>
2023-08-28 21:35:14 -0400

    Sección 2 28-08-23

--- app/Componentes/Sections/section2.jsx
+++ app/Componentes/Sections/section2.jsx
@@ -1,4 +1,4 @@
-import styles from '../Sections/section1.module.css'
+import styles from '../Sections/section2.module.css'
 import Image from 'next/image'
 
 const Nav = () =>{
@@ -6,18 +6,16 @@ const Nav = () =>{
        
       <div >  
 
-      <div className={styles.banner} >
-   <a href="cafes.php?id=7"><div className={styles.taza}></div></a>
-        <h6><span className={styles.oscuro}>Delicadamente irresistible</span></h6>
-        <h5><span className={styles.oscuro}>DESCUBRE LA</span> SENSIBILIDAD</h5>
-        <h5><span className={styles.oscuro}>DEL CAFÉ ORIGEN DEL QUINDÍO,</span></h5>
-        <h5><span className={styles.oscuro}>COLOMBIA</span></h5>
-        <div className={styles.btncontenedor}>
-        <button className={styles.button} >REGÍSTRATE</button>
-        <button className={styles.button2}>CONTÁCTANOS</button>
-        </div>
+<div className={styles.secbanner} >
+        <h6>Producción premium:</h6>       
+        <h5>EL CAFÉ COLOMBIANO</h5>
+        <h5>QUE EMOCIONA A EUROPA Y AL MUNDO.</h5>
+        <h6>Café de alta calidad con puntaje de <span className={styles.negrita}>84 en la escala (SCA).</span></h6>               
+        <button className={styles.buttonhome}>ENCUENTRA UN LUGAR CERCA</button>
         
         </div>
+
+      
         </div>
     )
   
