d13848ab3f1d424c661f02b33a32b90bf87046c1
Jesús Villegas <villegasvjd@gmail.com>
2023-08-29 21:17:38 -0400

    Sección 3 29-08-23

---
+++ app/Componentes/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from './footer.module.css'
+import Image from 'next/image'
+
+const Footer = ()=>{
+    return (
+        <div>
+            <Nav/>
+        </div>
+    )
+}
+
+export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './Componentes/Header/Header'
 import Section1 from './Componentes/Sections/section1' 
 import Section2 from './Componentes/Sections/section2' 
+import Section3 from './Componentes/Sections/section3'
 
 const Page = () => {
   return (
@@ -9,6 +10,7 @@ const Page = () => {
       <Header/>
       <Section1/>
       <Section2/>
+      <Section3/>
     </div>
   )
 }
