d25cc3f5cd986d1b0bdb34fa9fea57a70c55e6f6
Jesús Villegas <villegasvjd@gmail.com>
2023-08-29 22:10:27 -0400

    Footer 29-08-23

--- app/Componentes/Footer/Footer.jsx
+++ app/Componentes/Footer/Footer.jsx
@@ -4,7 +4,50 @@ import Image from 'next/image'
 const Footer = ()=>{
     return (
         <div>
-            <Nav/>
+
+<div  className={styles.footerc}>
+    <div>
+   <p>Copyright Macondo 2023</p>
+   <h3>PÁGINA OFICIAL DE MACONDO</h3>
+   <p>RUC 25485648544</p><br/>
+   <a href="#">
+    TERMINOS Y CONDICIONES
+   </a>
+   <a href="#">
+    POLÍTICAS DE PRIVACIDAD
+   </a>   
+   <a href="#">
+    CONTÁCTANOS
+   </a>
+   </div>
+   <div className={styles.bloqderfot}>
+    <div>
+    <a href="#"><span>CONTACTO</span></a>
+    <a href="#"><span>SOPORTE</span></a>
+    <a href="#"><span>ENVÍOS</span></a>
+    <a href="#"><span>COOKIES</span></a>
+    <a href="#"><span>SUSCRÍBETE</span></a>
+    </div>
+    <div>
+    <a href="#"><span>F.A.Q.</span></a>
+    <a href="#"><span>DEVOLUCIONES</span></a>    
+    <a href="#"><span>EMPLEOS</span></a>
+    <a href="#"><span>ÁREA PRIVADA</span></a>
+    <a href="#"><span>BLOG</span></a>    
+    </div>
+    <div>
+    <a href="#"><span>HISTORIA</span></a>
+    <a href="#"><span>NUESTRA SEDE</span></a>    
+    <a href="#"><span>CAFETERÍA</span></a>
+    <a href="#"><span>SOCIAL</span></a>
+    <a href="#"><span>MI CUENTA</span></a>
+    </div>
+    
+   </div>
+
+</div>
+
+
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Header from './Componentes/Header/Header'
 import Section1 from './Componentes/Sections/section1' 
 import Section2 from './Componentes/Sections/section2' 
 import Section3 from './Componentes/Sections/section3'
+import Footer from './Componentes/Footer/Footer'
 
 const Page = () => {
   return (
@@ -11,6 +12,7 @@ const Page = () => {
       <Section1/>
       <Section2/>
       <Section3/>
+      <Footer/>
     </div>
   )
 }
