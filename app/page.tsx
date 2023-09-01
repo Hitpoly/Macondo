11e8fa1359997e82b2403c623cdade24acacf04d
Jesús Villegas <villegasvjd@gmail.com>
2023-08-31 20:21:24 -0400

    Update 31-08-23

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Section1 from './Componentes/Sections/section1'
 import Section2 from './Componentes/Sections/section2' 
 import Section3 from './Componentes/Sections/section3'
 import Footer from './Componentes/Footer/Footer'
-import { useRef } from 'react';
+
 
 
   
@@ -12,12 +12,12 @@ import { useRef } from 'react';
 
 const Page = () => {
 
-  const url = location.href;
+  
 
 
   return (
     <div>
-         You are currently accessing <b>{url}</b>
+        
       <Header/>
       <Section1/>
       <Section2/>
